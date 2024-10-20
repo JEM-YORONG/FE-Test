4a2f21e5796ad2ac494ae0806f99846049db9f9a
JEM <yorongjemar0@gmail.com>
2024-10-20 08:13:54 +0800

    Added body content

--- src/components/layout/Body.tsx
+++ src/components/layout/Body.tsx
@@ -1,5 +1,8 @@
 import styles from "../../App.module.css";
 import { ReactComponent as MainImg } from "../../svg/body/pic1.svg";
+import { ReactComponent as Bell } from "../../svg/body/bell.svg";
+import { ReactComponent as Ball } from "../../svg/ball.svg";
+import { ReactComponent as Line } from "../../svg/body/line.svg";
 
 function Body() {
   return (
@@ -7,7 +10,49 @@ function Body() {
       <div className={styles.bodyContainer}>
         <MainImg className={styles.mainImg} />
         <div className={styles.announcement}>
-          <p>Announcement</p>
+          <Bell className={styles.bell} />
+          <p>¡FELICIDADES artxxxxipa! GANADOR DESTACADO</p>
+        </div>
+        <div className={styles.navContainer}>
+          <div className={styles.groupC}>
+            <Ball className={styles.fIcon} />
+            <p>SEARCH</p>
+          </div>
+          <Line className={styles.lineC} />
+          <div className={styles.category}>
+            <div className={styles.groupC}>
+              <Ball className={styles.fIcon} />
+              <p>START</p>
+            </div>
+            <div className={styles.groupC}>
+              <Ball className={styles.fIcon} />
+              <p>NEW</p>
+            </div>
+            <div className={styles.groupC}>
+              <Ball className={styles.fIcon} />
+              <p>SLOTS</p>
+            </div>
+            <div className={styles.groupC}>
+              <Ball className={styles.fIcon} />
+              <p>LIVE</p>
+            </div>
+            <div className={styles.groupC}>
+              <Ball className={styles.fIcon} />
+              <p>JACKPOTS</p>
+            </div>
+            <div className={styles.groupC}>
+              <Ball className={styles.fIcon} />
+              <p>TABLE GAMES</p>
+            </div>
+            <div className={styles.groupC}>
+              <Ball className={styles.fIcon} />
+              <p>BINGO</p>
+            </div>
+            <div className={styles.groupC}>
+              <Ball className={styles.fIcon} />
+              <p>OTHERS</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
